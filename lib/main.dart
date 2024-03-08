03706bd94b631a6c5feca2076da5e5288c6d0e3b
Sygix <17769298+Sygix@users.noreply.github.com>
2024-03-08 18:36:40 +0100

    fix: root MyApp class

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:appwrite/appwrite.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Client client = Client()
-      .setEndpoint("https://cloud.appwrite.io/v1")
-      .setProject("65eb3c30edc0ee1934e6")
-      .setSelfSigned(status: true); // For self signed certificates, only use for development
-  Account account = Account(client);
-
-  runApp(MyApp(account: account));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Account account;
-  const MyApp({super.key, required this.account});
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    Client client = Client()
+        .setEndpoint("https://cloud.appwrite.io/v1")
+        .setProject("65eb3c30edc0ee1934e6")
+        .setSelfSigned(status: true); //! For self signed certificates, only use for development
+    Account account = Account(client);
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
