fa84e08d06ef97679280aac1134bd1d491488662
helloyanis <helloyanis@hotmail.com>
2024-03-11 09:18:49 +0100

    dart fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:appwrite/appwrite.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
