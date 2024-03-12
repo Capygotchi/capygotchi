a20fb4f1fbcad2c06c5ccd9403c8cc2705fff6e2
Pire <p.clement01@orange.fr>
2024-03-12 18:20:30 +0100

    fontfamily

--- lib/main.dart
+++ lib/main.dart
@@ -86,6 +86,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        fontFamily: 'Capriola',
       ),
       routerConfig: router,
     );
