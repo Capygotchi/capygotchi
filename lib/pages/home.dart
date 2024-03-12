2ab8b9a010aa5b7f4e1330e1441f063e72afdb20
Pire <p.clement01@orange.fr>
2024-03-12 17:34:04 +0100

    separation du footer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,7 @@ import 'package:capygotchi/pages/capybara_stats.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'capybara.dart';
+import 'home_footer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,21 +21,6 @@ class _HomePageState extends State<HomePage> {
     hunger: 100,
     happiness: 100,
   );
-
-  feed() {
-    setState(() {
-      if (myCapybara.hunger < 200) {
-        myCapybara.hunger += 10;
-      }
-    });
-  }
-  pet() {
-    setState(() {
-      if (myCapybara.happiness < 200) {
-        myCapybara.happiness += 10;
-      }
-    });
-  }
 
   signOut() {
     try {
@@ -82,42 +68,9 @@ class _HomePageState extends State<HomePage> {
 
           ),
           const Spacer(),
-          SizedBox(
-              height: 70,
-              child: Stack(children: [
-                Container(color: const Color(0xff8A6552), height: 80),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 50),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      ElevatedButton(
-                        onPressed: () {pet();},
-                        child: const Icon(
-                          Icons.pets,
-                          color: Colors.white,
-                          size: 40,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: ElevatedButton(
-                          onPressed: () {feed();},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xff8A6552),
-                            minimumSize: const Size(100, 100),
-                            fixedSize: const Size(100, 100),
-                          ),
-                          child: const Icon(Icons.food_bank,
-                              color: Colors.white, size: 40),
-                        ),
-                      ),
-
-                    ],
-                  ),
-                ),
-              ]))
+          HomeFooterWidget(
+            myCapybara: myCapybara,
+          ),
         ],
       ),
     );
