e50a78254b08cdf2ab823e9368463e662a920725
Pire <p.clement01@orange.fr>
2024-03-13 12:20:12 +0100

    mooving capy

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -19,8 +19,7 @@ class AppRouter {
                 final insideLoginPath = state.fullPath.toString().startsWith('/login');
                 if(authStatus == AuthStatus.unauthenticated && !insideLoginPath) {
                   print('home: unauthenticated or not in loginPath redirecting to /login');
-                  return '/login';
-                  //return null;
+                  return '/';
                 } else {
                   return null;
                 }
