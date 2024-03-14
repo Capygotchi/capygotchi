123caa043cd553d5144dd5767e7e0f26fa3c580b
Sygix <17769298+Sygix@users.noreply.github.com>
2024-03-14 15:44:07 +0100

    fix: monster

--- lib/core/infrastructure/database_api.dart
+++ lib/core/infrastructure/database_api.dart
@@ -14,7 +14,7 @@ class DatabaseAPI extends ChangeNotifier{
     _databases = databases;
   }
 
-  Future<Capybara> getMonster({
+  Future<Capybara?> getMonster({
     required String userId
   }) async {
     try {
@@ -26,71 +26,54 @@ class DatabaseAPI extends ChangeNotifier{
           ]
       );
 
+      if(document == null || document.documents.isEmpty) return null;
+
       final capybaraInfo = document.documents.first.data;
 
-      if(capybaraInfo['alive'] == true) {
-        Utils.logDebug(message: 'getMonster name result: ${capybaraInfo['name']}');
-        Utils.logDebug(message: 'getMonster color result: ${capybaraInfo['color']}');
-        Utils.logDebug(message: 'getMonster birthDate result: ${DateTime.parse(capybaraInfo['birthDate'])}');
-        Utils.logDebug(message: 'getMonster hunger result: ${capybaraInfo['hunger']}');
-        Utils.logDebug(message: 'getMonster happiness result: ${capybaraInfo['happiness']}');
-        Utils.logDebug(message: 'getMonster life result: ${capybaraInfo['life']}');
-        Utils.logDebug(message: 'getMonster alive result: ${capybaraInfo['alive']}');
-        Utils.logDebug(message: 'getMonster userId result: ${document.documents.first.$id}');
+      return Capybara(
+          name: capybaraInfo['name'],
+          color: CapyColor.values.byName(capybaraInfo['color']),
+          birthDate: DateTime.parse(capybaraInfo['birthDate']),
+          hunger: capybaraInfo['hunger'],
+          happiness: capybaraInfo['happiness'],
+          life: capybaraInfo['life'],
+          alive: capybaraInfo['alive'],
+          documentId: document.documents.first.$id
+      );
 
-        return Capybara(
-            name: capybaraInfo['name'],
-            color: CapyColor.values.byName(capybaraInfo['color']),
-            birthDate: DateTime.parse(capybaraInfo['birthDate']),
-            hunger: capybaraInfo['hunger'],
-            happiness: capybaraInfo['happiness'],
-            life: capybaraInfo['life'],
-            alive: capybaraInfo['alive'],
-            documentId: document.documents.first.$id
-        );
-      } else {
-        deleteMonster(capybara: Capybara(name: capybaraInfo['name'], color: CapyColor.values.byName(capybaraInfo['color']), documentId: document.documents.first.$id));
-        return createMonster(capybara: Capybara(name: 'Roger', color: CapyColor.brown, documentId: ID.unique()), userId: userId);
-      }
     } on AppwriteException catch(e) {
       Utils.logError(message: e);
-      return createMonster(capybara: Capybara(name: 'Roger', color: CapyColor.brown, documentId: ''), userId: userId);
+      return null;
     } finally {
       notifyListeners();
     }
   }
 
-  Future<Capybara> createMonster({
+  Future<void> createMonster({
     required Capybara capybara,
     required String userId
   }) async {
     try {
-      final isHavingMonster = await getMonster(userId: userId);
-      if(isHavingMonster.documentId == '') {
-        await _databases.createDocument(
-            databaseId: AppWriteConstants.databaseId,
-            collectionId: AppWriteConstants.collectionId,
-            documentId: ID.unique(),
-            data: {
-              'name': capybara.name,
-              'color': capybara.color.name,
-              'birthDate': capybara.birthDate.toIso8601String(),
-              'hunger': capybara.hunger,
-              'happiness': capybara.happiness,
-              'life': capybara.life,
-              'alive': capybara.alive,
-              'userId': userId
-            }
-        );
-      } else {
-        Utils.logDebug(message: "User have already a capybara");
-      }
+      await _databases.createDocument(
+        databaseId: AppWriteConstants.databaseId,
+        collectionId: AppWriteConstants.collectionId,
+        documentId: ID.unique(),
+        data: {
+          'name': capybara.name,
+          'color': capybara.color.name,
+          'birthDate': capybara.birthDate.toIso8601String(),
+          'hunger': capybara.hunger,
+          'happiness': capybara.happiness,
+          'life': capybara.life,
+          'alive': capybara.alive,
+          'userId': userId
+        }
+      );
     } on AppwriteException catch(e) {
       Utils.logError(message: e);
     } finally {
       notifyListeners();
     }
-    return capybara;
   }
 
   updateMonster({
@@ -99,11 +82,11 @@ class DatabaseAPI extends ChangeNotifier{
   }) async {
     try {
       final isHavingMonster = await getMonster(userId: userId);
-      if(isHavingMonster.documentId != '') {
+      if(isHavingMonster != null && isHavingMonster?.documentId != '') {
         await _databases.updateDocument(
             databaseId: AppWriteConstants.databaseId,
             collectionId: AppWriteConstants.collectionId,
-            documentId: capybara.documentId,
+            documentId: isHavingMonster.documentId,
             data: {
               'name': capybara.name,
               'color': capybara.color.name,
--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -30,37 +30,39 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      getMonster(context);
+      initMonster(context);
     });
     super.initState();
   }
 
-  /* createMonster() {
-    context.read<DatabaseAPI>().createMonster(
-        capybara: Capybara(
-            name: 'Michel', color: CapyColor.brownWithHat,
-            documentId: ID.unique()
-        ),
-        userId: '65f0a73e31fe27bbe0e0'
-    );
-  } */
-  getMonster(BuildContext context) async {
+  initMonster(BuildContext context) async {
     final database = context.read<DatabaseAPI?>();
-    final userName = context.read<User?>()?.userName;
-    if(database != null && userName != null) {
-      final newCapybara = await database.getMonster(userId: context.read<User>().userId);
-      if (!context.mounted) return;
-      context.read<Capybara>().updateCapybara(newCapybara);
+    final user = context.read<User?>();
+
+    if(database != null && user?.userName != null) {
+      final loadedCapybara = await database.getMonster(userId: context.read<User>().userId);
+      final newCapybara = Capybara(
+        name: 'Capybara',
+        color: CapyColor.brown,
+        documentId: '',
+      );
+
+      // No capybara found in database
+      if(loadedCapybara == null && context.mounted) {
+        await database.createMonster(capybara: newCapybara, userId: context.read<User>().userId);
+        if(!context.mounted) return;
+        context.read<Capybara>().updateCapybara(newCapybara);
+      }
+
+      // Capybara found is dead
+      if(loadedCapybara?.alive == false && loadedCapybara != null && context.mounted) {
+        database.deleteMonster(capybara: loadedCapybara);
+        await database.createMonster(capybara: newCapybara, userId: context.read<User>().userId);
+        if(!context.mounted) return;
+        context.read<Capybara>().updateCapybara(newCapybara);
+      }
     }
   }
-  // updateMonster() {
-  //   context.read<DatabaseAPI>().updateMonster(capybara: Capybara(
-  //       name: 'Michel', color: 'Black', documentId: '65f1ef2b8c6b95d10152', hunger: 80, life: 50),
-  //       userId: '65f0a73e31fe27bbe0e0');
-  // }
-  // deleteMonster() {
-  //   context.read<DatabaseAPI>().deleteMonster(capybara: Capybara(name: '', color: '', documentId: '65f1ef2b8c6b95d10152'));
-  // }
 
   @override
   Widget build(BuildContext context) {
